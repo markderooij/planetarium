2c0aaec97f0515cdf076ed04f4ebce4a10fbd060
mark <m.derooij1@rocva.nl>
2025-03-05 12:53:29 +0100

    web update

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PlanetController;
 use App\Http\Controllers\SolarSystemController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/', [PlanetController::class, 'index']);
 Route::get('/{planet}', [PlanetController::class, 'show']);
 Route::get('/solar-systems', [SolarSystemController::class, 'index']);
